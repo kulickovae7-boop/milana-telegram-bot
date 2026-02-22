8ed3185de9c99c3837088f8950dc9b89a727d371
kulickovae7-boop <kulickovae7@gmail.com>
2026-02-22 12:55:37 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import telebot
+
+# ВСТАВЬТЕ СЮДА ТОКЕН ОТ BOTFATHER
+TOKEN = "PASTE_YOUR_TOKEN_HERE"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+                     "Привет, Милана 💛\n"
+                     "Сегодня учим словенский!\n\n"
+                     "Напиши: rada berem")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    text = message.text.lower()
+
+    if "rada berem" in text:
+        bot.send_message(message.chat.id,
+                         "Молодец! 🌟\n"
+                         "Rada berem = Я люблю читать")
+    else:
+        bot.send_message(message.chat.id,
+                         "Попробуй написать: rada berem")
+
+bot.infinity_polling()
